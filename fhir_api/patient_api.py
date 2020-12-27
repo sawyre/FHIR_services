406f7ca9dc44fe45a33127b881688c4f5f133f3e
vkoschman <207955@niuitmo.ru>
2020-12-28 02:11:47 +0300

    scratch for emr_api added

--- fhir_api/patient_api.py
+++ fhir_api/patient_api.py
@@ -2,20 +2,20 @@ import uuid
 from datetime import datetime, timedelta
 from flask import jsonify, abort, request, Blueprint
 import requests
-from .sql_query_function import _get_resource_by_id, _get_resources_by_dict
-
+from .sql_query_converter import sql_query_by_dict, sql_query_by_id
 
 REQUEST_API = Blueprint('patient_api', __name__)
- # TODO: Вынести в глобальные и заменить на нужные
+# TODO: Вынести в глобальные и заменить на нужные
 CREATE_RESOURCE_SERVER = "https://hisgateway.herokuapp.com/panel/his_requests/"
 SEARCH_RESOURCE_SERVER = "https://hisgateway.herokuapp.com/panel/his_requests/"
+
 
 def get_blueprint():
     """Return the blueprint for the main app module"""
     return REQUEST_API
 
 
-@REQUEST_API.route('/create_patient', methods=['POST'])
+@REQUEST_API.route('/patient', methods=['POST'])
 def create_patient():
     """
     Create a patient request record
@@ -27,10 +27,9 @@ def create_patient():
     if not request.get_json():
         abort(400)
     data = request.get_json(force=True)
-    
+
     patient_dict = {}
     patient_dict["resourceType"] = "Patient"
-    patient_dict["identifier"] = [{"value": data["policyNumber"]}]
 
     # Имена могут меняться или фамилии (при женитьбе), поэтому список
     names_list_dict = [{}]
@@ -42,11 +41,11 @@ def create_patient():
     patient_dict["gender"] = data['gender']
     # Формат YYYY-MM-DD
     patient_dict["birthDate"] = data['birthDate']
-    
+
     # Список средств связи
     telecom_list_dict = [{}]
     telecom_list_dict[0]["system"] = "phone"
-    telecom_list_dict[0]["value"] = data['phoneNumber']
+    telecom_list_dict[0]["value"] = data['phone_number']
     patient_dict["telecom"] = telecom_list_dict
 
     # Список адресов
@@ -60,24 +59,15 @@ def create_patient():
     print(ans.json())
     return patient_dict, 201
 
+
 @REQUEST_API.route('/patient/<string:_id>', methods=['GET'])
 def get_patient_by_id(_id):
     """
     Get a patient request record
     """
-    ans = _get_resource_by_id('patient', _id)
-    
-    return ans.json(), 201
+    sql_search_query = sql_query_by_id('patient', _id)
+    query_dict = {'query': sql_search_query}
+    print(query_dict)
+    ans = requests.post(SEARCH_RESOURCE_SERVER, headers={'Content-type': 'application/json'}, json=query_dict)
 
-@REQUEST_API.route('/get_patient', methods=['POST'])
-def get_patient():
-    """
-    Get a patient request record
-    """
-    if not request.get_json():
-        abort(400)
-    data = request.get_json(force=True)
-    search_dict = {"identifier": [{"value": str(data["policyNumber"])}]}    
-    patient_dict = _get_resources_by_dict('patient', search_dict)
-    
-    return patient_dict, 201
+    return query_dict, 201
