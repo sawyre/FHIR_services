96d01c5305bac5e8d693a151048b8d7b99bf846f
sawyre <sawure@mail.ru>
2020-12-28 11:18:05 +0300

    Revert "Revert "Revert "Revert "добавлен файл с функциями для обращения в базу данных""""
    This reverts commit 51fc0f1284ab5c7645b873eb91cdac0ea2fac25e.

--- fhir_api/sql_query_converter.py
+++
@@ -1,6 +0,0 @@
-def sql_query_by_id(resource_type, id):
-    query = 'select * from {} where txid={}'.format(resource_type, id)
-    return query
-
-def sql_query_by_dict(resource_type, fhir_resource_dict):
-    pass
